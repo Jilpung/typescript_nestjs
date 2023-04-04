4e4be9273c2862b289da927828674257a64c681c
Jilpung <bodkl5500@gmail.com>
2023-04-04 18:38:07 +0900

    day05

--- portfolio/day05/backend/index.js
+++ portfolio/day05/backend/index.js
@@ -2,9 +2,11 @@ import express from "express";
 import swaggerUi from "swagger-ui-express";
 import swaggerJSDoc from "swagger-jsdoc";
 import { options } from "./swagger/config.js";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerJSDoc(options)));
 
